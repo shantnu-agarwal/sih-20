665e4608f2ecdff824c580393d3563bf020672e3
Shantnu Agarwal <shantnu.agarwal@gmail.com>
2020-01-20 16:47:36 +0530

    server test 1

--- application.py
+++ application.py
@@ -2,7 +2,7 @@ import nltk
 from nltk.stem.lancaster import LancasterStemmer
 stemmer = LancasterStemmer()
 
-from flask import Flask
+from flask import *
 
 
 import numpy
@@ -17,87 +17,24 @@ import mysql.connector
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
-print("Hey")
-
 with open("intents.json") as file:
 	data = json.load(file)
-
-
-try:
-	with open("data.pickle","rb") as f:
-		words, labels, training, output = pickle.load(f)
-
-except:
-	words = []
-	labels = []
-	docs_x = []
-	docs_y = []
-
-	for intent in data["intents"]:
-		for pattern in intent["patterns"]:
-			wrds = nltk.word_tokenize(pattern)
-			words.extend(wrds)
-			docs_x.append(wrds)
-			docs_y.append(intent["tag"])
-
-		if intent["tag"] not in labels:
-			labels.append(intent["tag"])
-
-	words = [stemmer.stem(w.lower()) for w in words if w != "?"]
-	words = sorted(list(set(words)))
-
-	labels = sorted(labels)
-
-	training = []
-	output = []
-
-	out_empty = [0 for _ in range(len(labels))]
-
-	for x, doc in enumerate(docs_x):
-		bag = []
-
-		wrds = [stemmer.stem(w.lower()) for w in doc]
-
-		for w in words:
-			if w in wrds:
-				bag.append(1)
-			else:
-				bag.append(0)
-
-		output_row = out_empty[:]
-		output_row[labels.index(docs_y[x])] = 1
-
-		training.append(bag)
-		output.append(output_row)
-
-
-	training = numpy.array(training)
-	output = numpy.array(output)
-
-	with open("data.pickle","wb") as f:
-		pickle.dump((words, labels, training, output), f)
+with open("data.pickle","rb") as f:
+	words, labels, training, output = pickle.load(f)
 
 tensorflow.reset_default_graph()
-
 net = tflearn.input_data(shape=[None, len(training[0])])
 net = tflearn.fully_connected(net, 8)
 net = tflearn.fully_connected(net, 8)
 net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
 net = tflearn.regression(net)
-
 model = tflearn.DNN(net)
+model.load("model.tflearn")
+print("MODEL LOADED")
 
-try:
-	model.load("model.tflearn")
-except:
-	model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
-	model.save("model.tflearn")
+
 def bag_of_words(s, words):
 	bag = [0 for _ in range(len(words))]
-
 	s_words = nltk.word_tokenize(s)
 	s_words = [stemmer.stem(word.lower()) for word in s_words]
 
@@ -109,31 +46,41 @@ def bag_of_words(s, words):
 	return numpy.array(bag)
 
 
-def chat():
-	print("Start talking with the bot (type quit to stop)!")
-	while True:
-		inp = input("You: ")
-		if inp.lower() == "quit":
-			break
+def chat(inp):
+	if inp.lower() == "quit":
+		return("BYE")
+	results = model.predict([bag_of_words(inp, words)])
+	results_index = numpy.argmax(results)
+	tag = labels[results_index]
 
-		results = model.predict([bag_of_words(inp, words)])
-		results_index = numpy.argmax(results)
-		tag = labels[results_index]
+	for tg in data["intents"]:
+		if tg['tag'] == tag:
+			responses = tg['responses']
+	rsp = random.choice(responses)
+	print(type(rsp))
+	print(rsp)
+	if rsp=="rice" or rsp=="potato":
+		cnx = mysql.connector.connect(user="astroller27@kisaan2020", password="WeGotThis101", host="kisaan2020.mysql.database.azure.com", port=3306, database="world")
+		# print(cnx)
+		curs = cnx.cursor()
+		curs.execute(("select CAST(price as CHAR(20)) as col1 from crops where name = '{}';").format(rsp))
+		for row in curs:
+			for col in row:
+				print(col)
+				return col
+	else:
+		return(rsp)
 
-		for tg in data["intents"]:
-			if tg['tag'] == tag:
-				responses = tg['responses']
-		rsp = random.choice(responses)
-		print(type(rsp))
-		if rsp.isdigit() == True:
-			cnx = mysql.connector.connect(user="astroller27@kisaan2020", password="WeGotThis101", host="kisaan2020.mysql.database.azure.com", port=3306, database="world")
-			# print(cnx)
-			curs = cnx.cursor()
-			curs.execute(("Select cost_price from crops where id = '{}';").format(int(rsp)))
-			for answer in curs:
-				print(answer)
-				break
-		else:
-			print(rsp)
+@app.route("/")
+def hello():
+	return "Hello World!"
 
-chat()
+@app.route("/userinput",methods = ['POST'])
+def userinput():
+	if request.method == 'POST':
+		user = request.form['nm']
+		reply = chat(user)
+		return render_template("bot.html",error=reply)
+
+print("All prerequisites passed. Starting application now.")
+app.run(debug=True)
